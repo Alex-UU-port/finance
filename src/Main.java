7eb59709180d5ee9accd735ddde42e97dbec6bf8
AlexMosk <jamos2108@gmail.com>
2025-11-08 15:46:25 +0800

    2 commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,57 @@
-
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    static List<User> users;
+
     public static void main(String[] args) {
 
+        users = User.fromCSV();
+        //инициализация пользователей
+
+        while (true) {
+
+            System.out.println("Введите \"1\" для авторизации; \n" +
+                    "Введите \"2\" для регистрации; \n" +
+                    "Введите \"0\" для выхода");
+            int cmd = scanner.nextInt();
+
+            if (cmd == 0) {
+                toExit();
+                break;
+            } else if (cmd == 1) {
+                for (User user : users) {
+                    System.out.println(user.getLogin());
+                }
+            } else if (cmd == 2) {
+                toRegister();
+            } else {
+                System.out.println("Ошибка, неизвестная команда!");
+            }
+        }
+    }
+
+    public static void toRegister() {
+        System.out.println("Введите логин: ");
+        String log = scanner.next();
+
+        System.out.println("Введите пароль: ");
+        String pass = scanner.next();
+
+        User user = new User(log, pass);
+
+        users.add(user);
+
+    }
+
+    public static void toExit() {
+        User.saveCSV(users);
     }
 }
+
--- src/User.java
+++ src/User.java
@@ -1,13 +1,67 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class User {
     private String login;
     private String password;
-    private Wallet wallet;
+    //private Wallet wallet;
 
     public User (String login, String password) {
         this.login = login;
         this.password = password;
-        this.wallet = new Wallet();
+        //this.wallet = new Wallet();
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String toCSVString() {
+        return login + ";" + password + ";";
+    }
+
+    public static void saveCSV(List<User> users) {
+        String path = "users.csv";
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(path))) {
+            // Записываем данные
+            for (User user : users) {
+                writer.write(user.toCSVString());
+                writer.newLine();
+            }
+            System.out.println("Данные успешно сохранены в " + path);
+        } catch (IOException e) {
+            System.out.println("Ошибка при записи файла: " + e.getMessage());
+        }
+    }
+
+    public static List<User> fromCSV() {
+        String path = "users.csv"; // путь к вашему файлу CSV
+        List<User> users = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line;
+
+            while ((line = br.readLine()) != null) {
+
+                String[] fields = line.split(";"); // разделяем по точке с запятой
+                if (fields.length == 2) {
+                    String login = fields[0];
+                    String password  = fields[1];
+
+                    User user = new User(login, password);
+                    users.add(user);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка при чтении файла: " + e.getMessage());
+        }
+        return users;
     }
 
 }
